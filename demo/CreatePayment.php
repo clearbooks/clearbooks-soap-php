66568e9c82632d42d201b9640df07b98dd07711b
Lajos Gerecs <glptks@gmail.com>
2015-06-19 16:33:52 +0100

    Update CreatePayment.php

--- demo/CreatePayment.php
+++ demo/CreatePayment.php
@@ -1,6 +1,12 @@
 <?php
 require_once __DIR__ . '/includes.php';
 
+$invoice = new \Clearbooks_Soap_1_0_PaymentInvoice();
+$invoice->id = 212;
+$invoice->amount = 15.5;
+$allocatedToInvoices = [ $invoice ];
+
+
 // create the payment
 $payment = new \Clearbooks_Soap_1_0_Payment();
 $payment->amount         = 29.99;
@@ -8,7 +14,7 @@ $payment->accountingDate = date( 'Y-m-d' );
 $payment->bankAccount    = 7502001;
 $payment->description    = 'Invoice Payment';
 $payment->entityId       = 22;
-$payment->invoices       = 212;
+$payment->invoices       = $allocatedToInvoices;
 $payment->paymentMethod  = 1;
 $payment->type           = 'sales';
 
