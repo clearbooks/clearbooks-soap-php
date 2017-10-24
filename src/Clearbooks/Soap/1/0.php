dfbcd9713e7064de00ce887e88acf0afb5981524
Horváth Péter <peter@clearbooks.co.uk>
2017-10-24 13:54:55 +0200

    remove dodgy _call method and use magic methods

--- src/Clearbooks/Soap/1/0.php
+++ src/Clearbooks/Soap/1/0.php
@@ -4,6 +4,40 @@
  * @version 1.0
  * @package Clearbooks
  * @subpackage Soap/1
+ *
+ * @method \Clearbooks_Soap_1_0_ResponseStatus allocatePayment( \Clearbooks_Soap_1_0_AllocateQuery $query )
+ * @method \Clearbooks_Soap_1_0_AccountCodeResult createAccountCode( \Clearbooks_Soap_1_0_AccountCodeRequest $code )
+ * @method void addBankStatementLines( string $bankAccount, string $statementName, \Clearbooks_Soap_1_0_BankStatementLine[] $statementLines )
+ * @method void writeOffPartial( \Clearbooks_Soap_1_0_CreditPartialQuery $query )
+ * @method int createEntity( \Clearbooks_Soap_1_0_Entity $entity )
+ * @method \Clearbooks_Soap_1_0_InvoiceReturn createInvoice( \Clearbooks_Soap_1_0_Invoice $invoice )
+ * @method \Clearbooks_Soap_1_0_InvoiceReturn[] createInvoices( \Clearbooks_Soap_1_0_Invoice[] $invoices )
+ * @method \Clearbooks_Soap_1_0_JournalReturn createJournal( \Clearbooks_Soap_1_0_Journal $project )
+ * @method \Clearbooks_Soap_1_0_PaymentReturn createPayment( \Clearbooks_Soap_1_0_Payment $payment )
+ * @method \Clearbooks_Soap_1_0_PaymentReturn createPayments( \Clearbooks_Soap_1_0_Payment[] $payments )
+ * @method \Clearbooks_Soap_1_0_ProjectReturn createProject( \Clearbooks_Soap_1_0_Project $project )
+ * @method bool deleteEntity( int $entityId )
+ * @method bool deleteJournal( int $journalId )
+ * @method int getEntityIdFromExternalId( string $externalId )
+ * @method \Clearbooks_Soap_1_0_EntityOutstandingBalance getEntityOutstandingBalance( int $entityId, string $type )
+ * @method float getExchangeRate( \Clearbooks_Soap_1_0_ExchangeRateRequest $request )
+ * @method int getInvoiceIdFromInvoiceNumber( string $type, string $invoicePrefix, string $invoiceNumber )
+ * @method int getPaymentIdFromExternalId( string $externalId )
+ * @method \Clearbooks_Soap_1_0_AccountCodeHeading[] listAccountCodeHeadings()
+ * @method \Clearbooks_Soap_1_0_AccountCode[] listAccountCodes()
+ * @method \Clearbooks_Soap_1_0_ListBankAccountsReturn listBankAccounts()
+ * @method \Clearbooks_Soap_1_0_Currency[] listCurrencies()
+ * @method \Clearbooks_Soap_1_0_Entity[] listEntities( \Clearbooks_Soap_1_0_EntityQuery $query )
+ * @method \Clearbooks_Soap_1_0_Invoice[] listInvoices( \Clearbooks_Soap_1_0_InvoiceQuery $query )
+ * @method \Clearbooks_Soap_1_0_ListOutstandingBalancesReturn[] listOutstandingBalances( string $type, int $limit )
+ * @method \Clearbooks_Soap_1_0_Project[] listProjects( int $offset )
+ * @method \Clearbooks_Soap_1_0_Theme[] listThemes()
+ * @method \Clearbooks_Soap_1_0_AccountCodeResult updateAccountCode( int $codeId, \Clearbooks_Soap_1_0_AccountCodeRequest $code )
+ * @method int updateEntity( int $entityId, \Clearbooks_Soap_1_0_Entity $entity )
+ * @method \Clearbooks_Soap_1_0_ProjectReturn updateProject( int $projectId, \Clearbooks_Soap_1_0_Project $project )
+ * @method \Clearbooks_Soap_1_0_ResponseStatus voidInvoice( \Clearbooks_Soap_1_0_InvoiceRef $invoice )
+ * @method \Clearbooks_Soap_1_0_ResponseStatus voidPayment( \Clearbooks_Soap_1_0_RemovePayment $payment )
+ * @method \Clearbooks_Soap_1_0_CreditResponseStatus writeOff( \Clearbooks_Soap_1_0_CreditQuery $query )
  */
 class Clearbooks_Soap_1_0 extends \SoapClient
 {
@@ -41,9 +75,22 @@ class Clearbooks_Soap_1_0 extends \SoapClient
         'ResponseStatus'                => 'Clearbooks_Soap_1_0_ResponseStatus',
         'Theme'                         => 'Clearbooks_Soap_1_0_Theme' );
 
+    /**
+     * @var string
+     */
     protected $apiKey;
+
+    /**
+     * @var string
+     */
     protected $namespace;
 
+    /**
+     * @param string $apiKey
+     * @param string $wsdl
+     * @param array $options
+     * @throws Exception
+     */
     public function __construct( $apiKey, $wsdl = '', $options = array() )
     {
         if ( !$apiKey ) {
@@ -79,305 +126,6 @@ class Clearbooks_Soap_1_0 extends \SoapClient
     }
 
     /**
-     * @param \Clearbooks_Soap_1_0_AllocateQuery $query
-     * @return \Clearbooks_Soap_1_0_ResponseStatus
-     */
-    public function allocatePayment( \Clearbooks_Soap_1_0_AllocateQuery $query )
-    {
-        return $this->_call( 'allocatePayment', $query );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_AccountCodeRequest $code
-     * @return \Clearbooks_Soap_1_0_AccountCodeResult
-     */
-    public function createAccountCode( \Clearbooks_Soap_1_0_AccountCodeRequest $code )
-    {
-        return $this->_call( 'createAccountCode', $code );
-    }
-
-    /**
-     * @param string $bankAccount
-     * @param string $statementName
-     * @param \Clearbooks_Soap_1_0_BankStatementLine[] $statementLines
-     */
-    public function addBankStatementLines( $bankAccount, $statementName, $statementLines )
-    {
-        $this->_call( 'addBankStatementLines', $bankAccount, $statementName, $statementLines );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_CreditPartialQuery $query
-     */
-    public function writeOffPartial( $query )
-    {
-        $this->_call( 'writeOffPartial', $query );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_Entity $entity
-     * @return int
-     */
-    public function createEntity( \Clearbooks_Soap_1_0_Entity $entity )
-    {
-        return $this->_call( 'createEntity', $entity );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_Invoice $invoice
-     * @return \Clearbooks_Soap_1_0_InvoiceReturn
-     */
-    public function createInvoice( \Clearbooks_Soap_1_0_Invoice $invoice )
-    {
-        return $this->_call( 'createInvoice', $invoice );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_Invoice[] $invoices
-     * @return \Clearbooks_Soap_1_0_InvoiceReturn[]
-     */
-    public function createInvoices( array $invoices )
-    {
-        return $this->_call( 'createInvoices', $invoices );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_Journal $project
-     * @return \Clearbooks_Soap_1_0_JournalReturn
-     */
-    public function createJournal( \Clearbooks_Soap_1_0_Journal $project )
-    {
-        return $this->_call( 'createJournal', $project );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_Payment $payment
-     * @return \Clearbooks_Soap_1_0_PaymentReturn
-     */
-    public function createPayment( \Clearbooks_Soap_1_0_Payment $payment )
-    {
-        return $this->_call( 'createPayment', $payment );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_Payment[] $payments
-     * @return \Clearbooks_Soap_1_0_PaymentReturn
-     */
-    public function createPayments( array $payments )
-    {
-        return $this->_call( 'createPayments', $payments );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_Project $project
-     * @return \Clearbooks_Soap_1_0_ProjectReturn
-     */
-    public function createProject( \Clearbooks_Soap_1_0_Project $project )
-    {
-        return $this->_call( 'createProject', $project );
-    }
-
-    /**
-     * @param int $entityId
-     * @return bool
-     */
-    public function deleteEntity( $entityId )
-    {
-        return $this->_call( 'deleteEntity', $entityId );
-    }
-
-    /**
-     * @param int $journalId
-     * @return bool
-     */
-    public function deleteJournal( $journalId )
-    {
-        return $this->_call( 'deleteJournal', $journalId );
-    }
-
-    /**
-     * @param string $externalId
-     * @return int
-     */
-    public function getEntityIdFromExternalId( $externalId )
-    {
-        return $this->_call( 'getEntityIdFromExternalId', $externalId );
-    }
-
-    /**
-     * @param int $entityId
-     * @param string $type
-     * @return \Clearbooks_Soap_1_0_EntityOutstandingBalance
-     */
-    public function getEntityOutstandingBalance( $entityId, $type )
-    {
-        return $this->_call( 'getEntityOutstandingBalance', $entityId, $type );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_ExchangeRateRequest $request
-     * @return float
-     */
-    public function getExchangeRate( $request )
-    {
-        return $this->_call( 'getExchangeRate', $request );
-    }
-
-    /**
-     * @param string $type
-     * @param string $invoicePrefix
-     * @param string $invoiceNumber
-     * @return int
-     */
-    public function getInvoiceIdFromInvoiceNumber( $type, $invoicePrefix, $invoiceNumber )
-    {
-        return $this->_call( 'getInvoiceIdFromInvoiceNumber', $type, $invoicePrefix, $invoiceNumber );
-    }
-
-    /**
-     * @param string $externalId
-     * @return int
-     */
-    public function getPaymentIdFromExternalId( $externalId )
-    {
-        return $this->_call( 'getPaymentIdFromExternalId', $externalId );
-    }
-
-    /**
-     * @return \Clearbooks_Soap_1_0_AccountCodeHeading[]
-     */
-    public function listAccountCodeHeadings()
-    {
-        return $this->_call( 'listAccountCodeHeadings' );
-    }
-
-    /**
-     * @return \Clearbooks_Soap_1_0_AccountCode[]
-     */
-    public function listAccountCodes()
-    {
-        return $this->_call( 'listAccountCodes' );
-    }
-
-    /**
-     * @return \Clearbooks_Soap_1_0_ListBankAccountsReturn
-     */
-    public function listBankAccounts()
-    {
-        return $this->_call( 'listBankAccounts' );
-    }
-
-    /**
-     * @return \Clearbooks_Soap_1_0_Currency[]
-     */
-    public function listCurrencies()
-    {
-        return $this->_call( 'listCurrencies' );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_EntityQuery $query
-     * @return \Clearbooks_Soap_1_0_Entity[]
-     */
-    public function listEntities( \Clearbooks_Soap_1_0_EntityQuery $query = null )
-    {
-        return $this->_call( 'listEntities', $query );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_InvoiceQuery $query
-     * @return \Clearbooks_Soap_1_0_Invoice[]
-     */
-    public function listInvoices( \Clearbooks_Soap_1_0_InvoiceQuery $query )
-    {
-        return $this->_call( 'listInvoices', $query );
-    }
-
-    /**
-     * @param string $type
-     * @param int $limit
-     * @return \Clearbooks_Soap_1_0_ListOutstandingBalancesReturn[]
-     */
-    public function listOutstandingBalances( $type, $limit = 10 )
-    {
-        return $this->_call( 'listOutstandingBalances', $type, $limit );
-    }
-
-    /**
-     * @param int $offset
-     * @return \Clearbooks_Soap_1_0_Project[]
-     */
-    public function listProjects( $offset = 0 )
-    {
-        return $this->_call( 'listProjects', $offset );
-    }
-
-    /**
-     * @return \Clearbooks_Soap_1_0_Theme[]
-     */
-    public function listThemes()
-    {
-        return $this->_call( 'listThemes' );
-    }
-
-    /**
-     * @param int $codeId
-     * @param \Clearbooks_Soap_1_0_AccountCodeRequest $code
-     * @return \Clearbooks_Soap_1_0_AccountCodeResult
-     */
-    public function updateAccountCode( $codeId, \Clearbooks_Soap_1_0_AccountCodeRequest $code )
-    {
-        return $this->_call( 'updateAccountCode', $codeId, $code );
-    }
-
-    /**
-     * @param int $entityId
-     * @param \Clearbooks_Soap_1_0_Entity $entity
-     * @return int
-     */
-    public function updateEntity( $entityId, \Clearbooks_Soap_1_0_Entity $entity )
-    {
-        return $this->_call( 'updateEntity', $entityId, $entity );
-    }
-
-    /**
-     * @param int $projectId
-     * @param \Clearbooks_Soap_1_0_Project $project
-     * @return \Clearbooks_Soap_1_0_ProjectReturn
-     */
-    public function updateProject( $projectId, \Clearbooks_Soap_1_0_Project $project )
-    {
-        return $this->_call( 'updateProject', $projectId, $project );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_InvoiceRef $invoice
-     * @return \Clearbooks_Soap_1_0_ResponseStatus
-     */
-    public function voidInvoice( \Clearbooks_Soap_1_0_InvoiceRef $invoice )
-    {
-        return $this->_call( 'voidInvoice', $invoice );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_RemovePayment $payment
-     * @return \Clearbooks_Soap_1_0_ResponseStatus
-     */
-    public function voidPayment( \Clearbooks_Soap_1_0_RemovePayment $payment )
-    {
-        return $this->_call( 'voidPayment', $payment );
-    }
-
-    /**
-     * @param \Clearbooks_Soap_1_0_CreditQuery $query
-     * @return \Clearbooks_Soap_1_0_CreditResponseStatus
-     */
-    public function writeOff( \Clearbooks_Soap_1_0_CreditQuery $query )
-    {
-        return $this->_call( 'writeOff', $query );
-    }
-
-    /**
      * {@inheritDoc}
      */
     public function __getLastResponse()
@@ -394,26 +142,13 @@ class Clearbooks_Soap_1_0 extends \SoapClient
     }
 
     /**
-     * @param $name
-     * @param null $parameter
-     * @param null $_
-     * @return mixed
-     */
-    protected function _call( $name, $parameter = null, $_ = null )
-    {
-        $args = func_get_args();
-        array_shift( $args );
-        return $this->__soapCall( $name, $args, array( 'uri' => $this->namespace, 'soapaction' => '#' . $name ) );
-    }
-
-    /**
-     * @param $string
+     * @param string $xml
      * @return string
      */
-    private function _formatXml( $string )
+    private function _formatXml( $xml )
     {
         $doc = new \DomDocument( '1.0' );
-        $doc->loadXML( $string );
+        $doc->loadXML( $xml );
         $doc->formatOutput = true;
         return $doc->saveXML();
     }
