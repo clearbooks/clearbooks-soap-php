8aaf2bca1345d0d254e4844a513f8240d1a2a38c
Horváth Péter <peter@clearbooks.co.uk>
2018-03-23 14:29:44 +0100

    new API method: listPaymentMethods

---
+++ src/Clearbooks/Soap/1/0/PaymentMethod.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Clear Books
+ * @version 1.0
+ * @package Clearbooks
+ * @subpackage Soap/1/0
+ */
+class Clearbooks_Soap_1_0_PaymentMethod
+{
+    /** @var int */
+    public $id;
+
+    /** @var string */
+    public $name;
+
+    /** @var bool */
+    public $isClosed;
+}
